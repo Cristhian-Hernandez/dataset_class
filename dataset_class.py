d40f0e12b2e9ee20f06be5f467719906ce389b90
Cristhian Hernandez <cristhi_her2000@hotmail.com>
2017-08-08 13:55:51 -0500

    Update dataset_class.py
    Added val_unique() and fixed a few comments.

--- dataset_class.py
+++ dataset_class.py
@@ -1,21 +1,25 @@
 class Dataset:
     def __init__(self, data):
         self.header = data[0]
-        self.data = data[1:]
+        self.data = data[1:] # Makes sure we get only the data, excluding the heading
 
     def __str__(self):
-        return str(self.data[:10])
+        return str(self.data[:10])  # Output first 10 lines of data when called or printed
 
     def column(self, label):
         column = []
         for count, val in enumerate(self.header):
             if label == val:
                 for i in self.data:
-                    column.append(i[count])
-        if label not in self.header:
+                    column.append(i[count])  # If the given label matches one in the header
+        if label not in self.header:         # this will output the whole column of data (of said label)
             return None
         return column
 
     def count_unique(self, label):
         unique = set(self.column(label))
-        return len(unique)
+        return len(unique)  # This outputs the number of unique items in a column
+
+    def val_unique(self, label):
+        unique = set(self.column(label))
+        return unique  # Returns the unique items found within a column
